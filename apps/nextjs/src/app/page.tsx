125f5a89fb357ecf7eaddffa5ac486d5f312dedf
toycrane <alwaysfun2183@gmail.com>
2023-08-28 16:11:19 +0900

    feat: shadcn theme 적용

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client";
+
+import { Button } from "@/components/button";
+
 export const runtime = "edge";
 
 export default function HomePage() {
   return (
     <main>
-      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-        Hello
-      </h1>
+      <Button onClick={() => console.log("hello")}>Hello</Button>
     </main>
   );
 }
