19f859ca2203edd2f3ebabbc9c640d68e5b1ef07
toycrane <alwaysfun2183@gmail.com>
2023-08-28 16:36:06 +0900

    chore: remove unnessesary files

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,24 +1,6 @@
-import type { Metadata } from "next";
-
 import "../styles/globals.css";
 
 import { cn } from "@/libs/tailwind";
-
-export const metadata: Metadata = {
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
-  openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
-  },
-};
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/button";
 
 export const runtime = "edge";
@@ -7,9 +5,7 @@ export const runtime = "edge";
 export default function HomePage() {
   return (
     <main className="container mx-auto w-full px-4 sm:px-6 lg:px-8">
-      <div className="flex items-center justify-center">
-        <Button onClick={() => console.log("hello")}>Copy Link</Button>
-      </div>
+      <div className="flex items-center justify-center">Hello</div>
     </main>
   );
 }
