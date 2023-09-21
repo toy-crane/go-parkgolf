7ccc060993281b592005b4e231877e7c3c2322b6
toycrane <alwaysfun2183@gmail.com>
2023-09-21 15:16:28 +0900

    feat: move position when clicked marker

--- apps/nextjs/src/app/locations/page.tsx
+++ apps/nextjs/src/app/locations/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import type { Location } from "@/types";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import type { Fetcher } from "swr";
@@ -11,9 +12,16 @@ const fetcher: Fetcher<Location[], string> = (url) =>
 
 const Locations = () => {
   const { data: locations } = useSWR("/api/locations", fetcher);
+  const [position, setPosition] = useState({
+    // 지도의 초기 위치
+    center: { lat: 37.564214, lng: 127.001699 },
+    // 지도 위치 변경시 panto를 이용할지에 대해서 정의
+    isPanto: false,
+  });
+
   return (
     <Map
-      center={{ lat: 37.564214, lng: 127.001699 }}
+      center={position.center}
       level={7}
       style={{ width: "100%", height: "100vh" }}
     >
@@ -21,6 +29,12 @@ const Locations = () => {
         <MapMarker
           key={name}
           position={{ lat: Number(address.y), lng: Number(address.x) }}
+          onClick={() => {
+            setPosition({
+              center: { lat: Number(address.y), lng: Number(address.x) },
+              isPanto: true,
+            });
+          }}
         />
       ))}
     </Map>
