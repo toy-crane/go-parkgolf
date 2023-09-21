d1a62ff9adc154e7b319d0b578307bb1422df4db
toycrane <alwaysfun2183@gmail.com>
2023-09-21 15:01:38 +0900

    feat: naver map -> kakao map으로 변경

--- apps/nextjs/src/app/locations/layout.tsx
+++ apps/nextjs/src/app/locations/layout.tsx
@@ -1,9 +1,17 @@
+import Script from "next/script";
+
 export const runtime = "edge";
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
-    <main className="container mx-auto contents h-full w-full px-4 sm:px-6 lg:px-8">
-      {props.children}
-    </main>
+    <>
+      <Script
+        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=6696e0ed688e7915643565b54b26d6fb&libraries=services,clusterer&autoload=false"
+        strategy="beforeInteractive"
+      />
+      <main className="container mx-auto contents h-full w-full px-4 sm:px-6 lg:px-8">
+        {props.children}
+      </main>
+    </>
   );
 }
--- apps/nextjs/src/app/locations/page.tsx
+++ apps/nextjs/src/app/locations/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import { Map } from "@/components/maps/map";
-import { NavermapsProvider } from "react-naver-maps";
+import type { Location } from "@/types";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import type { Fetcher } from "swr";
+import useSWR from "swr";
+
+//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
+const fetcher: Fetcher<Location[], string> = (url) =>
+  fetch(url).then((res) => res.json());
 
 const Locations = () => {
+  const { data: locations } = useSWR("/api/locations", fetcher);
   return (
-    <NavermapsProvider ncpClientId={process.env.NEXT_PUBLIC_NCP_CLIENT_ID!}>
-      <Map />
-    </NavermapsProvider>
+    <Map
+      center={{ lat: 37.564214, lng: 127.001699 }}
+      level={7}
+      style={{ width: "100%", height: "100vh" }}
+    >
+      {locations?.map(({ name, address }) => (
+        <MapMarker
+          key={name}
+          position={{ lat: Number(address.y), lng: Number(address.x) }}
+        />
+      ))}
+    </Map>
   );
 };
 
