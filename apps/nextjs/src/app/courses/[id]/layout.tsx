2c6abb81998385fb8c1e2c95c068722d1c0f6398
toycrane <alwaysfun2183@gmail.com>
2023-10-11 13:56:45 +0900

    feat: add metadata to location detail

--- apps/nextjs/src/app/courses/[id]/layout.tsx
+++ apps/nextjs/src/app/courses/[id]/layout.tsx
@@ -1,3 +1,6 @@
+import type { Metadata } from "next";
+import { fetchCourse } from "@/libs/fetch";
+
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <div className="container mx-auto w-full px-4 sm:px-6 lg:px-8">
--- apps/nextjs/src/app/courses/[id]/page.tsx
+++ apps/nextjs/src/app/courses/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { promises as fs } from "fs";
 import path from "path";
+import type { Metadata } from "next";
+import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { notFound } from "next/navigation";
 import { fetchCourse } from "@/libs/fetch";
 import type { Course } from "@/types";
 
 import CourseDetail from "./card";
+
+interface Props {
+  params: { id: string };
+}
 
 export async function generateStaticParams() {
   const jsonDirectory = path.join(process.cwd(), "resource");
@@ -18,9 +24,20 @@ export async function generateStaticParams() {
   }));
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const course = await fetchCourse(params.id);
+  if (course) {
+    return {
+      title: course.name,
+      description: course.address.address_name,
+    };
+  } else {
+    return {};
+  }
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const course = await fetchCourse(params.id);
-
   if (course) {
     return <CourseDetail course={course} />;
   }
