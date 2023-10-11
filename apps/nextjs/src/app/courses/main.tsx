b7bc71f9f8261577e0f2703444d89f91fcb8059d
toycrane <alwaysfun2183@gmail.com>
2023-10-11 15:53:03 +0900

    feat: default position 변경

--- apps/nextjs/src/app/courses/main.tsx
+++ apps/nextjs/src/app/courses/main.tsx
@@ -27,7 +27,7 @@ import { Map } from "react-kakao-maps-sdk";
 
 const DEFAULT_POSITION = {
   level: 7,
-  center: { lat: 37.564214, lng: 127.001699 },
+  center: { lat: 37.5161996814031, lng: 127.075939572603 },
 };
 
 const Main = ({ courses }: { courses: Course[] }) => {
