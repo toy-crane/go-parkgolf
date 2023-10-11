098edf4144c64f1e948b518789519db568ffa412
toycrane <alwaysfun2183@gmail.com>
2023-10-11 17:35:10 +0900

    feat: remove exclaim mark

--- apps/nextjs/src/config/site.ts
+++ apps/nextjs/src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "전국의 파크골프 정보를 한 곳에서! | 파크골프가자",
   description:
-    "전국의 파크골프 코스와 정보를 쉽고 편하게 한 곳에서 찾아보세요. 가장 빠르게 성장하는 파크골프 커뮤니티, 파크골프가자!",
+    "전국의 파크골프 코스와 정보를 쉽고 편하게 한 곳에서 찾아보세요. 가장 빠르게 성장하는 파크골프 커뮤니티, 파크골프가자",
   url: "https://www.goparkgolf.app",
   ogImage: "https://www.goparkgolf.app/api/og",
   author: "toy-crane",
