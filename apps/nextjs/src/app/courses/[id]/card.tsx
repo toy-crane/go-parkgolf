22e75ec255496218e551b93c17f7a3ee3167ab1b
toycrane <alwaysfun2183@gmail.com>
2023-10-18 15:43:35 +0900

    feat: InfoNeeded 컴포넌트 추가

--- apps/nextjs/src/app/courses/[id]/card.tsx
+++ apps/nextjs/src/app/courses/[id]/card.tsx
@@ -15,11 +15,22 @@ interface CardProps {
   content: string | React.ReactNode;
 }
 
-const Description = ({ title, content }: CardProps) => {
+const Label = ({ title, content }: CardProps) => {
   return (
     <div className="flex items-center">
       <h2 className="mr-4 text-lg font-semibold">{title}</h2>
       <p className="text-muted-foreground text-lg">{content}</p>
+    </div>
+  );
+};
+
+const InfoNeeded = ({ href }: { href: string }) => {
+  return (
+    <div className="flex items-center">
+      <div className="mr-2">정보를 입력해 주세요</div>
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <Pencil size={16} />
+      </a>
     </div>
   );
 };
@@ -99,29 +110,35 @@ const CourseDetail = ({ course }: { course: Course }) => {
           {course.name}
         </h1>
         <div className="flex flex-col gap-1">
-          <Description title="위치" content={course.address.address_name} />
-          <Description title="규모" content={`${course?.hole_count} 홀`} />
+          <Label title="위치" content={course.address.address_name} />
+          <Label title="규모" content={`${course?.hole_count} 홀`} />
         </div>
         <Separator className="my-5" />
         <h2 className="text-foreground mb-2 text-xl font-bold">운영 시간</h2>
         <div className="flex flex-col gap-1">
-          <Description
+          <Label
             title="영업시간"
             content={
-              course?.operation.opening_hours ?? "등록된 정보가 없습니다"
+              course?.operation.opening_hours ??
+              InfoNeeded({
+                href: generateFormUrl(course.name),
+              })
             }
           />
-          <Description
+          <Label
             title="정기 휴무일"
             content={
-              course?.operation.regular_closed_days ?? "등록된 정보가 없습니다"
+              course?.operation.regular_closed_days ??
+              InfoNeeded({
+                href: generateFormUrl(course.name),
+              })
             }
           />
         </div>
         <Separator className="my-5" />
         <h2 className="text-foreground mb-2 text-xl font-bold">연락처</h2>
         <div className="flex flex-col gap-1">
-          <Description
+          <Label
             title="담당자 전화번호"
             content={
               course?.contact.phone_number ? (
@@ -132,7 +149,9 @@ const CourseDetail = ({ course }: { course: Course }) => {
                   {course?.contact.phone_number}
                 </a>
               ) : (
-                "등록된 연락처가 없습니다"
+                InfoNeeded({
+                  href: generateFormUrl(course.name),
+                })
               )
             }
           />
@@ -140,18 +159,27 @@ const CourseDetail = ({ course }: { course: Course }) => {
         <Separator className="my-5" />
         <h2 className="text-foreground mb-2 text-xl font-bold">이용 방법</h2>
         <div className="flex flex-col gap-1">
-          <Description
+          <Label
             title="예약 방법"
             content={
-              course?.operation.registration_method ?? "등록된 정보가 없습니다"
+              course?.operation.registration_method ??
+              InfoNeeded({
+                href: generateFormUrl(course.name),
+              })
             }
           />
-          <Description
+          <Label
             title="홈페이지"
             content={
-              <a href={course?.operation.website} className="text-blue-400">
-                상세 페이지 바로가기
-              </a>
+              course?.operation.website ? (
+                <a href={course?.operation.website} className="text-blue-400">
+                  상세 페이지 바로가기
+                </a>
+              ) : (
+                InfoNeeded({
+                  href: generateFormUrl(course.name),
+                })
+              )
             }
           />
         </div>
