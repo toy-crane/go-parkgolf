96ec72262efee07578724259a2d7dd7ebcc17104
toycrane <alwaysfun2183@gmail.com>
2023-10-18 15:21:26 +0900

    feat: detail page layout 수정

--- apps/nextjs/src/app/courses/[id]/card.tsx
+++ apps/nextjs/src/app/courses/[id]/card.tsx
@@ -18,6 +18,20 @@ import {
 } from "lucide-react";
 import { StaticMap } from "react-kakao-maps-sdk";
 
+interface CardProps {
+  title: string;
+  content: string | React.ReactNode;
+}
+
+const Description = ({ title, content }: CardProps) => {
+  return (
+    <div className="flex items-center">
+      <h2 className="mr-4 text-lg font-semibold">{title}</h2>
+      <p className="text-muted-foreground text-lg">{content}</p>
+    </div>
+  );
+};
+
 const CourseDetail = ({ course }: { course: Course }) => {
   const { track } = useAmplitude();
   const router = useRouter();
@@ -30,14 +44,6 @@ const CourseDetail = ({ course }: { course: Course }) => {
           <Button variant="ghost" size="icon" onClick={() => router.back()}>
             <ArrowLeft size={24} />
           </Button>
-          <div className="flex-col gap-4">
-            <h2 className="text-foreground text-lg font-semibold">
-              {course.name}
-            </h2>
-            <p className="text-muted-foreground text-sm">
-              {course.address.address_name}
-            </p>
-          </div>
         </div>
         <div>
           <Button
@@ -74,7 +80,7 @@ const CourseDetail = ({ course }: { course: Course }) => {
         </div>
       </div>
       <StaticMap // 지도를 표시할 Container
-        className="mb-6"
+        className="mb-12"
         marker={[
           {
             position: {
@@ -96,69 +102,66 @@ const CourseDetail = ({ course }: { course: Course }) => {
         }}
         level={6} // 지도의 확대 레벨
       />
-      <div className="mb-20 grid w-full items-center">
-        <div className="flex items-center gap-4">
-          <FlagTriangleRight size={20} />
-          <div className="text-base">{course?.hole_count}홀</div>
+      <div className="mb-20 flex w-full flex-col">
+        <h1 className="text-foreground mb-4 text-4xl font-bold">
+          {course.name}
+        </h1>
+        <div className="flex flex-col gap-1">
+          <Description title="위치" content={course.address.address_name} />
+          <Description title="규모" content={`${course?.hole_count} 홀`} />
         </div>
-        <Separator className="my-2" />
-        <div className="flex items-center gap-4">
-          <Phone size={20} />
-          <div className="text-base">
-            {course?.contact.phone_number ? (
-              <Button
-                variant="link"
-                size="sm"
-                asChild
-                className="p-0 text-base text-blue-400"
-                onClick={() => {
-                  track("phone number clicked", { ...course });
-                }}
-              >
-                <a href={`tel:${course?.contact.phone_number}`}>
+        <Separator className="my-5" />
+        <h2 className="text-foreground mb-2 text-xl font-bold">운영 시간</h2>
+        <div className="flex flex-col gap-1">
+          <Description
+            title="영업시간"
+            content={
+              course?.operation.opening_hours ?? "등록된 정보가 없습니다"
+            }
+          />
+          <Description
+            title="정기 휴무일"
+            content={
+              course?.operation.regular_closed_days ?? "등록된 정보가 없습니다"
+            }
+          />
+        </div>
+        <Separator className="my-5" />
+        <h2 className="text-foreground mb-2 text-xl font-bold">연락처</h2>
+        <div className="flex flex-col gap-1">
+          <Description
+            title="담당자 전화번호"
+            content={
+              course?.contact.phone_number ? (
+                <a
+                  href={`tel:${course?.contact.phone_number}`}
+                  className="text-blue-400"
+                >
                   {course?.contact.phone_number}
                 </a>
-              </Button>
-            ) : (
-              "등록된 연락처가 없습니다"
-            )}
-          </div>
+              ) : (
+                "등록된 연락처가 없습니다"
+              )
+            }
+          />
         </div>
-        <Separator className="my-2" />
-        <div className="flex items-center gap-4">
-          <Clock size={20} />
-          <div className="text-base">
-            <div>
-              영업 시간 -{" "}
-              {course?.operation.opening_hours ?? "등록된 정보가 없습니다"}
-            </div>
-            {course?.operation.regular_closed_days && (
-              <div> 정기 휴무일 - {course?.operation.regular_closed_days}</div>
-            )}
-          </div>
-        </div>
-        <Separator className="my-2" />
-        <div className="flex items-center gap-4">
-          <AlarmClock size={20} />
-          <div className="text-base">
-            <div>
-              예약 방법 -{" "}
-              {course?.operation.registration_method ??
-                "등록된 정보가 없습니다"}
-            </div>
-            <div>
-              {course?.operation.website ? (
-                <Button
-                  variant="link"
-                  size="sm"
-                  asChild
-                  className="p-0 text-base text-blue-400"
-                >
-                  <a href={course?.operation.website}>상세 정보 홈페이지</a>
-                </Button>
-              ) : null}
-            </div>
-          </div>
+        <Separator className="my-5" />
+        <h2 className="text-foreground mb-2 text-xl font-bold">이용 방법</h2>
+        <div className="flex flex-col gap-1">
+          <Description
+            title="예약 방법"
+            content={
+              course?.operation.registration_method ?? "등록된 정보가 없습니다"
+            }
+          />
+          <Description
+            title="홈페이지"
+            content={
+              <a href={course?.operation.website} className="text-blue-400">
+                상세 페이지 바로가기
+              </a>
+            }
+          />
         </div>
       </div>
     </div>
