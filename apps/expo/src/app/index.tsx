39c0b17dfb3442905eb81f00976588e7b372000a
toycrane <alwaysfun2183@gmail.com>
2023-10-25 12:51:15 +0900

    Add SafeAreaProvider and SafeAreaView to Home screen

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { Linking } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 
 import { originAllowList, serviceAllowList } from "~/configs/origin-allow-list";
@@ -8,22 +9,26 @@ export default function Home() {
   const webViewRef = useRef<WebView>(null);
 
   return (
-    <WebView
-      webviewDebuggingEnabled
-      ref={webViewRef}
-      allowsBackForwardNavigationGestures
-      geolocationEnabled
-      originWhitelist={originAllowList}
-      onShouldStartLoadWithRequest={(request) => {
-        // Only allow navigating within this website
-        if (serviceAllowList.some((url) => request.url.startsWith(url))) {
-          return true;
-        } else {
-          void Linking.openURL(request.url);
-          return false;
-        }
-      }}
-      source={{ uri: process.env.EXPO_PUBLIC_WEB_URL! }}
-    />
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1 bg-transparent">
+        <WebView
+          webviewDebuggingEnabled
+          ref={webViewRef}
+          allowsBackForwardNavigationGestures
+          geolocationEnabled
+          originWhitelist={originAllowList}
+          onShouldStartLoadWithRequest={(request) => {
+            // Only allow navigating within this website
+            if (serviceAllowList.some((url) => request.url.startsWith(url))) {
+              return true;
+            } else {
+              void Linking.openURL(request.url);
+              return false;
+            }
+          }}
+          source={{ uri: process.env.EXPO_PUBLIC_WEB_URL! }}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
