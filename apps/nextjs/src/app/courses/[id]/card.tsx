42c5216edc6e026421398e614e9441b22e98439d
toycrane <alwaysfun2183@gmail.com>
2023-10-30 18:37:56 +0900

    Remove unnecessary target and rel attributes from region link

--- apps/nextjs/src/app/courses/[id]/card.tsx
+++ apps/nextjs/src/app/courses/[id]/card.tsx
@@ -214,8 +214,6 @@ const CourseDetail = ({ course }: { course: Course }) => {
             <Link
               href={`/?level=${region.level}&lng=${region.lng}&lat=${region.lat}`}
               key={region.name}
-              target="_black"
-              rel="noopener noreferrer"
             >
               {region.name} 지역
             </Link>
