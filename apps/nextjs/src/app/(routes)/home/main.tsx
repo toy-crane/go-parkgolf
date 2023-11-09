415cb2d630ea06cc998d951fe0124cd3a0f298c8
toycrane <alwaysfun2183@gmail.com>
2023-11-09 22:12:12 +0900

    chore: 코드 정리 및 주석 제거

--- apps/nextjs/src/app/(routes)/home/main.tsx
+++ apps/nextjs/src/app/(routes)/home/main.tsx
@@ -1,31 +1,4 @@
-"use client";
-
-import { useEffect, useMemo, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet";
-import { useToast } from "@/components/ui/use-toast";
-import { DEFAULT_POSITION } from "@/config/map";
-import { useAmplitude } from "@/libs/amplitude";
-import { generateFormUrl } from "@/libs/google-form";
-import { cn } from "@/libs/tailwind";
 import type { Course, Position } from "@/types";
-import {
-  AlarmClock,
-  Clock,
-  FlagTriangleRight,
-  Pencil,
-  Phone,
-  Share2,
-} from "lucide-react";
 
 import CourseSheet from "./course-sheet";
 import Footer from "./footer";
@@ -34,57 +7,17 @@ import MainMap from "./main-map";
 
 const Main = ({
   courses,
-  level,
-  lat,
-  lng,
   modalOpen,
   selectedCourse,
+  position,
 }: {
   courses: Course[];
   selectedCourse?: Course;
-  level?: string;
-  lat?: string;
-  lng?: string;
   modalOpen: boolean;
+  position: Position;
 }) => {
-  // 지도의 위치
-  const [position, setPosition] = useState<Position>({
-    level: DEFAULT_POSITION.level,
-    center: {
-      lat: DEFAULT_POSITION.center.lat,
-      lng: DEFAULT_POSITION.center.lng,
-    },
-  });
-
-  const address = selectedCourse?.address[0];
-
-  useEffect(() => {
-    if (selectedCourse) {
-      const lat = address?.y!;
-      const lng = address?.x!;
-      setPosition((p) => ({
-        ...p,
-        center: {
-          lat,
-          lng,
-        },
-      }));
-    }
-  }, [address?.x, address?.y, selectedCourse]);
-
-  useEffect(() => {
-    setPosition((p) => ({
-      ...p,
-      level: level ? Number(level) : p.level,
-      center: {
-        lat: lat ? Number(lat) : p.center.lat,
-        lng: lng ? Number(lng) : p.center.lng,
-      },
-    }));
-  }, [lat, lng, level]);
-
   return (
-    <>
+    <main>
       <Header courses={courses} />
       <section>
         <MainMap
@@ -95,7 +28,7 @@ const Main = ({
       </section>
       <Footer />
       <CourseSheet selectedCourse={selectedCourse} open={modalOpen} />
-    </>
+    </main>
   );
 };
 
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import Main from "@/app/(routes)/home/main";
+import { DEFAULT_POSITION } from "@/config/map";
 import { createFetch } from "@/libs/cache";
 import type { Database } from "@/types/generated";
 import type { DbResult, DbResultOk } from "@/types/supabase-helper";
@@ -18,6 +19,9 @@ const Home = async ({
 }) => {
   const cookieStore = cookies();
   const selectedCourseId = Number(searchParams?.courseId) || undefined;
+  const level = Number(searchParams?.level) || DEFAULT_POSITION.level;
+  const lat = Number(searchParams?.lat) || undefined;
+  const lng = Number(searchParams?.lng) || undefined;
   const modalOpen = searchParams?.modal === "true";
 
   const supabase = createRouteHandlerClient<Database>(
@@ -34,17 +38,27 @@ const Home = async ({
 
   if (result.data) {
     const courses: DbResultOk<typeof query> = result.data;
+
     const selectedCourse = courses.find(
       (course) => course.id === selectedCourseId,
     );
+
+    const selectedCourseLat = selectedCourse?.address[0]?.y;
+    const selectedCourseLng = selectedCourse?.address[0]?.x;
+
+    const position = {
+      level,
+      center: {
+        lat: selectedCourseLat ?? lat ?? DEFAULT_POSITION.center.lat,
+        lng: selectedCourseLng ?? lng ?? DEFAULT_POSITION.center.lng,
+      },
+    };
     return (
       <Main
         courses={courses}
         selectedCourse={selectedCourse}
-        level={searchParams.level}
-        lat={searchParams.lat}
-        lng={searchParams.lng}
         modalOpen={modalOpen}
+        position={position}
       />
     );
   }
