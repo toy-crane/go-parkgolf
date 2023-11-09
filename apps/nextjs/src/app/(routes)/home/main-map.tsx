27b8c0e3f5ace6d2b7e50c9a69dad41806982c4e
toycrane <alwaysfun2183@gmail.com>
2023-11-09 22:36:41 +0900

    feat(main-map): 지도 드래그 이벤트 핸들러 추가

--- apps/nextjs/src/app/(routes)/home/main-map.tsx
+++ apps/nextjs/src/app/(routes)/home/main-map.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import Marker from "@/app/(routes)/home/marker";
 import { useAmplitude } from "@/libs/amplitude";
 import type { Course, Position } from "@/types";
 import { Map } from "react-kakao-maps-sdk";
+import { useDebouncedCallback } from "use-debounce";
 
 interface Props {
   courses: Course[];
@@ -15,13 +16,26 @@ interface Props {
 
 const MainMap = ({ courses, selectedCourse, position }: Props) => {
   const { track } = useAmplitude();
+  const searchParams = useSearchParams();
   const router = useRouter();
+  const pathname = usePathname();
+
+  const handlePosition = useDebouncedCallback((map: kakao.maps.Map) => {
+    const lng = map.getCenter().getLng();
+    const lat = map.getCenter().getLat();
+    const params = new URLSearchParams(searchParams);
+    params.set("lng", String(lng));
+    params.set("lat", String(lat));
+    router.replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <Map
       center={position.center}
       isPanto={true}
       level={position.level}
       style={{ width: "100%", height: "100vh" }}
+      onDrag={handlePosition}
     >
       {courses?.map((course) => (
         <Marker
