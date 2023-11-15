9cc5b469baad334150f23ae242a9096c6d3132bc
toycrane <alwaysfun2183@gmail.com>
2023-11-15 17:42:02 +0900

    feat(home/footer): add tracking for button clicks

--- apps/nextjs/src/app/(routes)/home/footer.tsx
+++ apps/nextjs/src/app/(routes)/home/footer.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/libs/tailwind";
+import { useAmplitude } from "@/libs/amplitude";
 
 const Footer = () => {
+  const { track } = useAmplitude();
   return (
     <footer>
       <nav className="fixed bottom-0 left-0 right-0 z-30 flex justify-end px-3 pb-3">
@@ -13,6 +16,7 @@ const Footer = () => {
               href="https://play.google.com/store/apps/details?id=app.goparkgolf.www"
               target="_blank"
               rel="noopener noreferrer"
+              onClick={() => track("android app install button clicked")}
             >
               <Image
                 width={16}
@@ -29,6 +33,7 @@ const Footer = () => {
               href="https://forms.gle/41DvTTg1Z3SrQpNQ8"
               target="_blank"
               rel="noopener noreferrer"
+              onClick={() => track("contact button clicked")}
             >
               제작자에게 문의하기
             </a>
