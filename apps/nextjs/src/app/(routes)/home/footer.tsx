ac5b101bd1f1ed2da39a2f1d91cb51464ede099c
toycrane <alwaysfun2183@gmail.com>
2023-11-15 17:41:02 +0900

    feat(home/footer): Add Google Play app install button

--- apps/nextjs/src/app/(routes)/home/footer.tsx
+++ apps/nextjs/src/app/(routes)/home/footer.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/libs/tailwind";
 
 const Footer = () => {
   return (
     <footer>
       <nav className="fixed bottom-0 left-0 right-0 z-30 flex justify-end px-3 pb-3">
-        <h2>
-          <Button className="font-bold" asChild size="sm">
+        <div className="flex flex-col gap-2">
+          <Button className="font-bold" asChild size="default">
+            <a
+              href="https://play.google.com/store/apps/details?id=app.goparkgolf.www"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                width={16}
+                height={16}
+                className="mr-2"
+                src={"/icons/google-play.svg"}
+                alt="google play icon"
+              />
+              안드로이드 앱 설치
+            </a>
+          </Button>
+          <Button className="font-bold" asChild size="default">
             <a
               href="https://forms.gle/41DvTTg1Z3SrQpNQ8"
               target="_blank"
@@ -15,7 +33,7 @@ const Footer = () => {
               제작자에게 문의하기
             </a>
           </Button>
-        </h2>
+        </div>
       </nav>
     </footer>
   );
