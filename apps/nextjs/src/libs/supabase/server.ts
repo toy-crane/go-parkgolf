201ad92220097f02b857754d0fb9910beec106cb
toycrane <alwaysfun2183@gmail.com>
2023-11-29 17:46:05 +0900

    fix: Fix environment variable references in Supabase server client

--- apps/nextjs/src/libs/supabase/server.ts
+++ apps/nextjs/src/libs/supabase/server.ts
@@ -8,8 +8,8 @@ export function createSupabaseServerClient() {
   const cookieStore = cookies();
 
   return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name: string) {
