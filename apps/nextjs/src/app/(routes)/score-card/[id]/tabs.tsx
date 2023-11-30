6e0162432f75faae957adbe594980bff91f8bfb7
toycrane <alwaysfun2183@gmail.com>
2023-11-30 13:52:12 +0900

    refactor(tabs): separate column and data formatting functions

--- apps/nextjs/src/app/(routes)/score-card/[id]/tabs.tsx
+++ apps/nextjs/src/app/(routes)/score-card/[id]/tabs.tsx
@@ -7,23 +7,17 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ScoreCard } from "./score-card";
 import type { GameCourse } from "./type";
 
-export const ScoreTabs = ({
-  gameCourses,
-  selectedTab,
-}: {
-  gameCourses: GameCourse[];
-  selectedTab?: string;
-}) => {
-  const [tab, setTab] = useState(selectedTab ?? gameCourses[0]?.name!);
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const scores = gameCourses.find((gc) => gc.name === tab)?.score ?? [];
-  const participants = scores[0]?.player_score.map((p) => p.participant) ?? [];
+const getColumnNames = (gameCourses: GameCourse[]) => {
+  const participants =
+    gameCourses[0]?.score[0]?.player_score.map((p) => p.participant) || [];
   const columns = participants?.map((p) => ({
     accessorKey: String(p?.id),
     headerName: p?.nickname ?? "이름 없음",
   }));
+  return columns;
+};
 
+const getFormattedData = (gameCourses: GameCourse[]) => {
   const data = gameCourses
     .map((gc) =>
       gc.score.map((score) => {
@@ -48,6 +42,21 @@ export const ScoreTabs = ({
       }),
     )
     .flat();
+  return data;
+};
+
+export const ScoreTabs = ({
+  gameCourses,
+  selectedTab,
+}: {
+  gameCourses: GameCourse[];
+  selectedTab?: string;
+}) => {
+  const [tab, setTab] = useState(selectedTab ?? gameCourses[0]?.name!);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const columns = getColumnNames(gameCourses);
+  const data = getFormattedData(gameCourses);
 
   const handleTabChange = (value: string) => {
     const params = new URLSearchParams(searchParams);
