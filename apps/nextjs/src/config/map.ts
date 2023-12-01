d5028469eb34629a4c63a3a2a7a2efa5af337884
toycrane <alwaysfun2183@gmail.com>
2023-12-01 10:45:23 +0900

    chore(config/map): update default map position

--- apps/nextjs/src/config/map.ts
+++ apps/nextjs/src/config/map.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_POSITION = {
-  level: 10,
-  center: { lat: 37.5161996814031, lng: 127.075939572603 },
+  level: 12,
+  center: { lat: 36.45425017380932, lng: 127.45513335136509 },
 };
