9910456c47aac7b09bd4c039f8d78cd21fd593e9
toycrane <alwaysfun2183@gmail.com>
2023-12-06 14:42:34 +0900

    feat(nav): add AuthNav and HomeNav components

---
+++ apps/nextjs/src/components/nav/auth.tsx
@@ -0,0 +1,24 @@
+"use client;";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const AuthNav = () => {
+  return (
+    <header className="content-grid z-header fixed h-[var(--header-height)] w-full border-b bg-white">
+      <nav className="flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            width={40}
+            height={40}
+            alt="Logo"
+            className="align-middle"
+          />
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default AuthNav;
---
+++ apps/nextjs/src/components/nav/home.tsx
@@ -0,0 +1,33 @@
+"use client;";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "../ui/button";
+
+const HomeNav = () => {
+  return (
+    <header className="content-grid fixed h-[var(--header-height)] w-full border-b bg-white">
+      <nav className="breakout flex items-center justify-between">
+        <Link href={"/"} className="flex-shrink-0 self-center">
+          <Image
+            src="/logo.png"
+            width={40}
+            height={40}
+            alt="Logo"
+            className="align-middle"
+          />
+        </Link>
+        <Button
+          variant="outline"
+          size="sm"
+          className="text-secondary-foreground"
+        >
+          <Link href={"/register"}>로그인</Link>
+        </Button>
+      </nav>
+    </header>
+  );
+};
+
+export default HomeNav;
