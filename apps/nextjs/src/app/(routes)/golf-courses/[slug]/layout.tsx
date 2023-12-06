7ae78d83c459ffe4499b9493828ff8b59c3a001f
toycrane <alwaysfun2183@gmail.com>
2023-12-06 15:19:03 +0900

    feat(layout): add Kakao Maps script and container

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/layout.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/layout.tsx
@@ -1,8 +1,17 @@
+import Script from "next/script";
+import { env } from "@/env.mjs";
+
 const Layout = (props: { children: React.ReactNode }) => {
   return (
-    <div className="container mx-auto w-full px-4 sm:px-6 lg:px-8">
-      {props.children}
-    </div>
+    <>
+      <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&libraries=services,clusterer&autoload=false`}
+        strategy="beforeInteractive"
+      />
+      <div className="container mx-auto w-full px-4 sm:px-6 lg:px-8">
+        {props.children}
+      </div>
+    </>
   );
 };
 
