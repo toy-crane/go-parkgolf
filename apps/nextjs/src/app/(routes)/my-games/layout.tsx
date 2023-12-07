2e85ccc28b5de8abbfc83c8d6e2c599be1205e04
toycrane <alwaysfun2183@gmail.com>
2023-12-07 11:57:56 +0900

    style: remove GameNav component from my-games layout

--- apps/nextjs/src/app/(routes)/my-games/layout.tsx
+++ apps/nextjs/src/app/(routes)/my-games/layout.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import BottomNav from "@/components/nav/bottom";
-import GameNav from "@/components/nav/game";
 import { useUserStore } from "@/libs/store/user";
 
 const Layout = (props: { children: React.ReactNode }) => {
@@ -10,7 +9,6 @@ const Layout = (props: { children: React.ReactNode }) => {
 
   return (
     <>
-      <GameNav />
       <main className="content-grid pb-[var(--bottom-nav-height)]">
         {props.children}
       </main>
