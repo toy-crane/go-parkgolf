f8034d0e8c8076bcc7d78ae9292be222d500d82c
toycrane <alwaysfun2183@gmail.com>
2023-12-07 11:53:35 +0900

    style: remove unnecessary semicolon and add bottom navigation

--- apps/nextjs/src/app/(routes)/my-games/layout.tsx
+++ apps/nextjs/src/app/(routes)/my-games/layout.tsx
@@ -1,10 +1,15 @@
+import BottomNav from "@/components/nav/bottom";
 import GameNav from "@/components/nav/game";
 
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <>
       <GameNav />
-      <main className="content-grid">{props.children}</main>;
+      <main className="content-grid pb-[var(--bottom-nav-height)]">
+        {props.children}
+      </main>
+      ;
+      <BottomNav />
     </>
   );
 };
