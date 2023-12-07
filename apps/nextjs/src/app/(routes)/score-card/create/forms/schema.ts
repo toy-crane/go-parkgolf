0a6422a0a3b531de3bc0d9f18d319cf92c522abf
toycrane <alwaysfun2183@gmail.com>
2023-12-07 16:05:17 +0900

    fix(forms/schema.ts): Fix error message for courseId

--- apps/nextjs/src/app/(routes)/score-card/create/forms/schema.ts
+++ apps/nextjs/src/app/(routes)/score-card/create/forms/schema.ts
@@ -4,7 +4,10 @@ export const gameSchema = z.object({
   startedAt: z.coerce.date({
     required_error: "게임 날짜를 선택해 주세요.",
   }),
-  courseId: z.coerce.number({ required_error: "골프장을 선택해 주세요." }),
+  courseId: z.coerce.number({
+    required_error: "골프장을 선택해 주세요.",
+    invalid_type_error: "골프장을 선택해 주세요.",
+  }),
 });
 
 export const gamePlayerSchema = z.object({
