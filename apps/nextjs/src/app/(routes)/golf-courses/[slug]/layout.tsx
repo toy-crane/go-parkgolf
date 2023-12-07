e53797759c51589d0e03462fc73e229b80194ac1
toycrane <alwaysfun2183@gmail.com>
2023-12-07 15:18:58 +0900

    feat(layout): add BottomNav component

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/layout.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/layout.tsx
@@ -1,7 +1,10 @@
+import BottomNav from "@/components/nav/bottom";
+
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <>
       <div className="content-grid">{props.children}</div>
+      <BottomNav />
     </>
   );
 };
