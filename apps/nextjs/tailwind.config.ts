6081a7c94a3f2d4481f3250f1217c3101fb92a53
toycrane <alwaysfun2183@gmail.com>
2023-12-08 09:49:04 +0900

    style: fix typo in tailwind.config.ts

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -87,7 +87,7 @@ export default {
         header: "var(--header-height)",
         "bottom-nav": "var(--bottom-nav-height)",
         "dynamic-screen": "100dvh",
-        "short-scrren": "100svh",
+        "short-screen": "100svh",
       },
     },
   },
