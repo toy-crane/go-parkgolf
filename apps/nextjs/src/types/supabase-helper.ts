d6391d79ecbcb1f33fe86862260ef72d351f5506
toycrane <alwaysfun2183@gmail.com>
2023-12-12 10:43:38 +0900

    refactor: Remove unnecessary code

--- apps/nextjs/src/types/supabase-helper.ts
+++ apps/nextjs/src/types/supabase-helper.ts
@@ -6,9 +6,3 @@ export type Tables<T extends keyof Database["public"]["Tables"]> =
   Database["public"]["Tables"][T]["Row"];
 export type Enums<T extends keyof Database["public"]["Enums"]> =
   Database["public"]["Enums"][T];
-
-export type DbResult<T> = T extends PromiseLike<infer U> ? U : never;
-export type DbResultOk<T> = T extends PromiseLike<{ data: infer U }>
-  ? Exclude<U, null>
-  : never;
-export type DbResultErr = PostgrestError;
