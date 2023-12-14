cf035d4f82d730b366d3443e1c63f20dc7d45d94
toycrane <alwaysfun2183@gmail.com>
2023-12-14 20:12:00 +0900

    feat: Update Supabase configuration

--- apps/expo/src/lib/supabase.ts
+++ apps/expo/src/lib/supabase.ts
@@ -3,9 +3,8 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://nlclqihmkqqmdmflexer.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sY2xxaWhta3FxbWRtZmxleGVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzMjQwODUsImV4cCI6MjAxNDkwMDA4NX0.2ApqEVKU-wsq1lrKYwlHjkZCW4U0zFPrnCceP_-8ZCQ";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
