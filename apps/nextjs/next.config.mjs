f5b5b3f2cc616fada6bbb1bd4e7d445edfff0735
toycrane <alwaysfun2183@gmail.com>
2023-12-22 10:59:15 +0900

    feat: add images domain configuration

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -7,6 +7,9 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    domains: ["k.kakaocdn.net"],
+  },
   logging: {
     fetches: {
       fullUrl: true,
