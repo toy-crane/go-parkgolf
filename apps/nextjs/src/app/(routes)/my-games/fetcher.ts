fb393c4115b09624ad8491ad57a06cb83acb7e35
toycrane <alwaysfun2183@gmail.com>
2023-12-27 16:45:40 +0900

    feat: add user authentication to getMyGames function

--- apps/nextjs/src/app/(routes)/my-games/fetcher.ts
+++ apps/nextjs/src/app/(routes)/my-games/fetcher.ts
@@ -1,12 +1,18 @@
+import { readUserSession } from "@/libs/auth";
 import { createSupabaseServerClientReadOnly } from "@/libs/supabase/server";
 import camelcaseKeys from "camelcase-keys";
 
 export const getMyGames = async () => {
+  const session = await readUserSession();
+
+  if (!session) throw new Error("User session not found");
+
   const supabase = await createSupabaseServerClientReadOnly();
 
   const { data: response, error } = await supabase
     .from("games")
     .select("*, golf_courses(*), game_courses(*), game_players(*)")
+    .eq("user_id", session.user.id)
     .in("status", ["in_progress", "completed"])
     .order("created_at", {
       ascending: false,
