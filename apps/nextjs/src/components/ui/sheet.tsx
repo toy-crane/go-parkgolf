8552aa1b21b3e33d5fc30530dcca058511dd5eeb
toycrane <alwaysfun2183@gmail.com>
2024-01-02 18:55:49 +0900

    style: improve sheet content scrollability

--- apps/nextjs/src/components/ui/sheet.tsx
+++ apps/nextjs/src/components/ui/sheet.tsx
@@ -33,7 +33,6 @@ const SheetOverlay = React.forwardRef<
     {...props}
     ref={ref}
   />
-  
 ));
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
@@ -58,27 +57,38 @@ const sheetVariants = cva(
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof sheetVariants> {
+  scrollable?: boolean;
+}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
-  <SheetPortal>
-    <SheetOverlay />
-    <SheetPrimitive.Content
-      ref={ref}
-      className={cn(sheetVariants({ side }), className)}
-      {...props}
-    >
-      {children}
-      <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-        <Cross2Icon className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close>
-    </SheetPrimitive.Content>
-  </SheetPortal>
-));
+>(({ side = "right", className, scrollable, children, ...props }, ref) => {
+  const SheetWrapper = scrollable ? SheetOverlay : React.Fragment;
+  return (
+    <SheetPortal>
+      {!scrollable && <SheetOverlay />}
+      <SheetWrapper {...(scrollable && { className: "overflow-y-auto" })}>
+        <SheetPrimitive.Content
+          ref={ref}
+          className={cn(
+            sheetVariants({ side }),
+            scrollable ? "relative" : "",
+            className,
+          )}
+          {...props}
+        >
+          {children}
+          <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+            <Cross2Icon className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </SheetPrimitive.Close>
+        </SheetPrimitive.Content>
+      </SheetWrapper>
+    </SheetPortal>
+  );
+});
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
 const SheetHeader = ({
