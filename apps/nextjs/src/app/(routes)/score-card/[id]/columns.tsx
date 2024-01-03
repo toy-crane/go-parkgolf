3ac0e4146fcaf9478d92cd72346c1eb9980ea9d1
toycrane <alwaysfun2183@gmail.com>
2024-01-03 15:55:08 +0900

    refactor(columns): Update footer text

--- apps/nextjs/src/app/(routes)/score-card/[id]/columns.tsx
+++ apps/nextjs/src/app/(routes)/score-card/[id]/columns.tsx
@@ -21,7 +21,7 @@ export const useGetColumns = (dynamicColumns: ColumnName[]) => {
       columnHelper.accessor("holeNumber", {
         cell: (info) => info.getValue(),
         header: "홀",
-        footer: "T",
+        footer: "총합",
       }),
       columnHelper.accessor("par", {
         cell: (info) => info.getValue(),
