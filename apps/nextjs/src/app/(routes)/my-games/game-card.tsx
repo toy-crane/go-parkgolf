0ecd9c20cd235dda60168e4ddc69df6e792ef853
toycrane <alwaysfun2183@gmail.com>
2024-01-04 15:12:03 +0900

    feat(game-card): Add game status badge and display game details

--- apps/nextjs/src/app/(routes)/my-games/game-card.tsx
+++ apps/nextjs/src/app/(routes)/my-games/game-card.tsx
@@ -3,25 +3,50 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { format } from "date-fns";
+import { ChevronRight } from "lucide-react";
 
-import DeleteAlert from "./delete-alert";
 import type { Game } from "./type";
+import { ko } from "date-fns/locale";
+
+type Status = "in_progress" | "completed" | "deleted" | "draft";
+
+const STATUS_LABEL: {
+  [key in Status]: {
+    text: string;
+    variant: "default" | "secondary" | "destructive" | "outline";
+  };
+} = {
+  in_progress: {
+    text: "진행중",
+    variant: "default",
+  },
+  completed: {
+    text: "게임완료",
+    variant: "secondary",
+  },
+  deleted: {
+    text: "취소",
+    variant: "destructive",
+  },
+  draft: {
+    text: "임시저장",
+    variant: "outline",
+  },
+};
 
 const GameCard = ({
-  game: { id, golfCourses, gameCourses, gamePlayers, startedAt },
+  game: { id, golfCourses, gameCourses, gamePlayers, startedAt, status },
 }: {
   game: Game;
 }) => {
+  const gameCourseNames = gameCourses
+    ?.map((gameCourse) => gameCourse.name)
+    .join(" ");
+  const gamePlayerNames = gamePlayers
+    .map((gamePlayer) => gamePlayer.nickname)
+    .join(" | ");
   return (
     <Card
       key={id}
@@ -30,37 +55,33 @@ const GameCard = ({
         redirect(`/score-card/${id}`);
       }}
     >
-      <CardHeader>
-        <CardTitle>
-          <Link href={`/score-card/${id}`}>{golfCourses?.name}</Link>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 border-b p-2 px-2">
+        <CardTitle className="flex items-center justify-between text-xs">
+          {format(new Date(startedAt), "yyyy-MM-dd (eee) HH:mm", {
+            locale: ko,
+          })}
         </CardTitle>
-        <CardDescription className="flex flex-col gap-1">
-          {format(new Date(startedAt), "yyyy-MM-dd")}
-        </CardDescription>
+        <Link
+          href={`/score-card/${id}`}
+          className="inline-flex h-6 items-center rounded-md px-2 text-xs font-medium text-neutral-600"
+        >
+          상세보기
+          <ChevronRight className="ml-0.5 h-3 w-3" />
+        </Link>
       </CardHeader>
-      <CardContent>
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-wrap gap-1 gap-y-1 self-start">
-            {gameCourses.map(({ id: gameCourseId, name }) => (
-              <Badge key={gameCourseId} variant="secondary">
-                <Link href={`/score-card/${id}?tab=${name}`}>{name} 코스</Link>
-              </Badge>
-            ))}
-          </div>
-          <div>
-            {gamePlayers.map((p) => p.nickname).length}인 (
-            {gamePlayers.map((p) => p.nickname).join(", ")})
-          </div>
-        </div>
+      <CardContent className="px-2 py-4 pt-2">
+        <Badge
+          variant={STATUS_LABEL[status].variant}
+          className="rounded-sm px-1 py-0 text-xs font-medium"
+        >
+          {STATUS_LABEL[status].text}
+        </Badge>
+        <h2 className="text-md mb-1 font-medium">
+          {golfCourses?.name} {golfCourses?.holeCount}홀
+        </h2>
+        <div className="text-xs text-gray-500">{gameCourseNames} 코스</div>
+        <div className="text-xs text-gray-500">{gamePlayerNames}</div>
       </CardContent>
-      <CardFooter className="flex flex-col gap-2">
-        <div className="flex gap-2 self-end">
-          <Button asChild variant="outline" size="sm">
-            <Link href={`/score-card/${id}`}>기록보기</Link>
-          </Button>
-          <DeleteAlert gameId={id} />
-        </div>
-      </CardFooter>
     </Card>
   );
 };
--- apps/nextjs/src/app/(routes)/my-games/type.ts
+++ apps/nextjs/src/app/(routes)/my-games/type.ts
@@ -2,7 +2,7 @@ import type { Tables } from "@/types/generated";
 import type { CamelCaseKeys } from "camelcase-keys";
 
 export type Game = CamelCaseKeys<Tables<"games">, true> & {
-  golfCourses: { name: string } | null;
+  golfCourses: { name: string; holeCount: number } | null;
   gameCourses: CamelCaseKeys<Tables<"game_courses">, true>[];
   gamePlayers: CamelCaseKeys<Tables<"game_players">, true>[];
 };
