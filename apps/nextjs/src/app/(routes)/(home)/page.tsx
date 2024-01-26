4dfb9ee046734be5664d03c7d6839b3799942978
toycrane <alwaysfun2183@gmail.com>
2024-01-26 14:22:29 +0900

    refactor(page.tsx): remove unused code

--- apps/nextjs/src/app/(routes)/(home)/page.tsx
+++ apps/nextjs/src/app/(routes)/(home)/page.tsx
@@ -2,8 +2,7 @@ import HomeNav from "@/components/nav/home";
 import { DEFAULT_POSITION } from "@/config/map";
 import { haversineDistance } from "@/libs/map";
 
-import { getCourses, getGolfCourseReviews } from "./_components/action";
-import CourseDetailSheet from "./_components/course-detail-sheet";
+import { getCourses } from "./_components/action";
 import Footer from "./_components/footer";
 import MainMap from "./_components/main-map";
 
@@ -24,14 +23,12 @@ const Home = async ({
   const level = Number(searchParams?.level) || DEFAULT_POSITION.level;
   const lat = Number(searchParams?.lat) || undefined;
   const lng = Number(searchParams?.lng) || undefined;
-  const modalOpen = searchParams?.modal === "true";
 
   const courses = await getCourses();
 
   const selectedCourse = courses.find(
     (course) => course.id === selectedCourseId,
   );
-  const reviews = await getGolfCourseReviews(selectedCourseId);
 
   const position = {
     level: level,
@@ -56,23 +53,6 @@ const Home = async ({
     }).toString()}`,
   }));
 
-  const allCoursesExcludeMe = courses.filter(
-    (course) => course.slug !== selectedCourse?.slug,
-  );
-
-  const nearCourses = allCoursesExcludeMe.filter((course) => {
-    const courseLat = course.lat ?? 0;
-    const courseLng = course.lng ?? 0;
-    return (
-      haversineDistance(
-        selectedCourse?.lat ?? 0,
-        selectedCourse?.lng ?? 0,
-        courseLat,
-        courseLng,
-      ) <= 20
-    );
-  });
-
   return (
     <>
       <HomeNav selectOptions={selectOptions} />
@@ -84,12 +64,6 @@ const Home = async ({
         />
       </section>
       <Footer />
-      <CourseDetailSheet
-        selectedCourse={selectedCourse}
-        nearCourses={nearCourses}
-        open={modalOpen}
-        reviews={reviews}
-      />
     </>
   );
 };
