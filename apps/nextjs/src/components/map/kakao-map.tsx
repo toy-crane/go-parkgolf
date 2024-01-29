5128164f461ff1f86c5c96dcf4ce74e80f1c2a61
toycrane <alwaysfun2183@gmail.com>
2024-01-29 14:48:26 +0900

    feat(map): add marker clustering feature

--- apps/nextjs/src/components/map/kakao-map.tsx
+++ apps/nextjs/src/components/map/kakao-map.tsx
@@ -3,7 +3,7 @@
 import { useRef } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { cn } from "@/libs/tailwind";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MarkerClusterer } from "react-kakao-maps-sdk";
 
 import CurrentPositionButton from "./current-position";
 import Marker from "./marker";
@@ -88,13 +88,42 @@ const KakaoMap = ({
         }}
         level={level ?? 5} // 지도의 확대 레벨
       >
-        {markers.map((marker) => (
-          <Marker
-            onClick={() => router.push(marker.to)}
-            {...marker}
-            key={marker.to}
-          />
-        ))}
+        <MarkerClusterer
+          averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+          minLevel={10} // 클러스터 할 최소 지도 레벨
+          calculator={[10, 30]} // 클러스터의 크기 구분 값, 각 사이값마다 설정된 text나 style이 적용된다
+          styles={[
+            {
+              // calculator 각 사이 값 마다 적용될 스타일을 지정한다
+              width: "40px",
+              height: "40px",
+              background: "#1bc8df",
+              borderRadius: "20px",
+              color: "#000",
+              textAlign: "center",
+              fontWeight: "bold",
+              lineHeight: "41px",
+            },
+            {
+              width: "50px",
+              height: "50px",
+              background: "#e6789c",
+              borderRadius: "25px",
+              color: "#000",
+              textAlign: "center",
+              fontWeight: "bold",
+              lineHeight: "51px",
+            },
+          ]}
+        >
+          {markers.map((marker) => (
+            <Marker
+              onClick={() => router.push(marker.to)}
+              {...marker}
+              key={marker.to}
+            />
+          ))}
+        </MarkerClusterer>
       </Map>
     </>
   );
