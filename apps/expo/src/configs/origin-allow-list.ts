7144475ee70ad7299c0387966cbbe106afc5e843
toycrane <alwaysfun2183@gmail.com>
2024-02-05 15:15:08 +0900

    refactor(expo): add new origin allow list entries

--- apps/expo/src/configs/origin-allow-list.ts
+++ apps/expo/src/configs/origin-allow-list.ts
@@ -3,6 +3,8 @@ export const PermittedDevicesAllowList: string[] = ["tel:*"];
 export const serviceAllowList: string[] = [
   "https://www.goparkgolf.app",
   "http://192.168.45.232:3000",
+  "https://accounts.kakao.com",
+  "http://k.kakaocdn.net",
 ];
 
 export const originAllowList: string[] = [
