2b2271759796c77fa0b64cf21aac00d6b6be5cf4
toycrane <alwaysfun2183@gmail.com>
2024-02-15 14:14:44 +0900

    style: simplify GameCard component structure

--- apps/nextjs/src/app/(routes)/my-games/game-card.tsx
+++ apps/nextjs/src/app/(routes)/my-games/game-card.tsx
@@ -55,34 +55,36 @@ const GameCard = ({
         redirect(`/score-card/${id}`);
       }}
     >
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 border-b p-2 px-2">
-        <CardTitle className="flex items-center justify-between text-xs">
-          {format(new Date(startedAt), "yyyy-MM-dd (eee) HH:mm", {
-            locale: ko,
-          })}
-        </CardTitle>
-        <Link
-          href={`/score-card/${id}`}
-          className="inline-flex h-6 items-center rounded-md px-2 text-xs font-medium text-neutral-600"
-        >
-          상세보기
-          <ChevronRight className="ml-0.5 h-3 w-3" />
-        </Link>
-      </CardHeader>
-      <CardContent className="px-2 py-4 pt-2">
-        <Badge
-          variant={STATUS_LABEL[status].variant}
-          className="rounded-sm px-1 py-0 text-xs font-medium"
-        >
-          {STATUS_LABEL[status].text}
-        </Badge>
-        <h2 className="text-md mb-1 font-medium">{golfCourses?.name}</h2>
-        <div className="text-xs text-gray-500">
-          {gameCourseNames} 코스<span> · </span>
-          {golfCourses?.holeCount}홀
-        </div>
-        <div className="text-xs text-gray-500">{gamePlayerNames}</div>
-      </CardContent>
+      <Link href={`/score-card/${id}`}>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 border-b p-2 px-2">
+          <CardTitle className="flex items-center justify-between text-xs">
+            {format(new Date(startedAt), "yyyy-MM-dd (eee) HH:mm", {
+              locale: ko,
+            })}
+          </CardTitle>
+          <Link
+            href={`/score-card/${id}`}
+            className="inline-flex h-6 items-center rounded-md px-2 text-xs font-medium text-neutral-600"
+          >
+            상세보기
+            <ChevronRight className="ml-0.5 h-3 w-3" />
+          </Link>
+        </CardHeader>
+        <CardContent className="px-2 py-4 pt-2">
+          <Badge
+            variant={STATUS_LABEL[status].variant}
+            className="rounded-sm px-1 py-0 text-xs font-medium"
+          >
+            {STATUS_LABEL[status].text}
+          </Badge>
+          <h2 className="text-md mb-1 font-medium">{golfCourses?.name}</h2>
+          <div className="text-xs text-gray-500">
+            {gameCourseNames} 코스<span> · </span>
+            {golfCourses?.holeCount}홀
+          </div>
+          <div className="text-xs text-gray-500">{gamePlayerNames}</div>
+        </CardContent>
+      </Link>
     </Card>
   );
 };
