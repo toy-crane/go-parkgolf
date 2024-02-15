6007c63f142960e9c5273bda7b1d54c6f4756d41
toycrane <alwaysfun2183@gmail.com>
2024-02-15 16:28:11 +0900

    refactor(page): rename trending page header text

--- apps/nextjs/src/app/(routes)/trending/page.tsx
+++ apps/nextjs/src/app/(routes)/trending/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <div>
       <PageHeader className="relative flex flex-row items-center justify-between pb-4 md:pb-8">
-        <PageHeaderHeading>파크골프 트렌딩</PageHeaderHeading>
+        <PageHeaderHeading>파크골프 트렌드</PageHeaderHeading>
       </PageHeader>
     </div>
   );
