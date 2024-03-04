6d248478cdbea253efdf6f3d93ba988eb7317500
toycrane <alwaysfun2183@gmail.com>
2024-03-04 18:58:47 +0900

    feat: Add dynamic route logic for creating a game

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/cta.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/cta.tsx
@@ -2,15 +2,16 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { isApp } from "@/libs/user-agent";
 import { track } from "@vercel/analytics/react";
 
 const CTA = ({ courseId }: { courseId: string }) => {
+  const routeUrl = isApp(navigator.userAgent)
+    ? `/score-card/create/golf-course?golfCourseId=${courseId}`
+    : "/score-card";
   return (
     <Button className="min-w-[320px]" asChild>
-      <Link
-        href={`/score-card/create/golf-course?golfCourseId=${courseId}`}
-        onClick={() => track("create game CTA clicked")}
-      >
+      <Link href={routeUrl} onClick={() => track("create game CTA clicked")}>
         파크골프 스코어 기록하기
       </Link>
     </Button>
