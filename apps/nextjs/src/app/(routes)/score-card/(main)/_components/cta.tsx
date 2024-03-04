0608ad2bcd8cf86eca06bb54eb0aaa88badeb66b
toycrane <alwaysfun2183@gmail.com>
2024-03-04 19:52:52 +0900

    refactor(routes): Remove unnecessary code and simplify CTA component

--- apps/nextjs/src/app/(routes)/score-card/(main)/_components/cta.tsx
+++ apps/nextjs/src/app/(routes)/score-card/(main)/_components/cta.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { isApp } from "@/libs/user-agent";
 import { track } from "@vercel/analytics/react";
 
 const CTA = () => {
-  return isApp(navigator.userAgent) ? (
-    <Button className="w-full" asChild>
-      <Link href="/score-card/create/golf-course">스코어 기록하기</Link>
-    </Button>
-  ) : (
+  return (
     <Button className="w-full font-bold" asChild size="sm" variant="secondary">
       <a
         href="https://play.google.com/store/apps/details?id=app.goparkgolf.www"
