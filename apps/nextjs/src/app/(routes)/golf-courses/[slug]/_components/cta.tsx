57b93d1224d010e697c3ca2dce56eb5d7a2a4b90
toycrane <alwaysfun2183@gmail.com>
2024-03-06 18:53:45 +0900

    feat(routes): Add courseName prop to CTA component

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/cta.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/cta.tsx
@@ -5,15 +5,24 @@ import { Button } from "@/components/ui/button";
 import { useUserAgentStore } from "@/libs/store/user-agent";
 import { track } from "@vercel/analytics/react";
 
-const CTA = ({ courseId }: { courseId: string }) => {
+const CTA = ({
+  courseId,
+  courseName,
+}: {
+  courseId: string;
+  courseName: string;
+}) => {
   const isMobileApp = useUserAgentStore((state) => state.isMobileApp);
   const route = isMobileApp
     ? `/score-card/create/golf-course?golfCourseId=${courseId}`
     : "/score-card";
   return (
-    <Button className="min-w-[320px]" asChild>
+    <Button className="mx-2 w-full" asChild>
       <Link href={route} onClick={() => track("create game CTA clicked")}>
-        파크골프 스코어 기록하기
+        <span className="hidden md:inline-flex">
+          {courseName} 스코어 기록하기
+        </span>
+        <span className="inline-flex md:hidden">파크골프 스코어 기록하기</span>
       </Link>
     </Button>
   );
--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/page.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/page.tsx
@@ -172,7 +172,7 @@ export default async function Page({ params, searchParams }: Props) {
       />
       <div className="z-bottom-nav content-grid fixed bottom-[var(--bottom-nav-height)] w-full justify-center bg-gradient-to-t from-white from-80% to-transparent pb-3">
         <div className="md:content full flex justify-center pt-5">
-          <CTA courseId={currentCourse.id} />
+          <CTA courseId={currentCourse.id} courseName={currentCourse.name} />
         </div>
       </div>
       <BottomNav />
