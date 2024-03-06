769e4bc2edca47b99fd7ca648a4b0f4c31482017
toycrane <alwaysfun2183@gmail.com>
2024-03-06 14:19:53 +0900

    feat: Update CTA component with dynamic route handling

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/cta.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/cta.tsx
@@ -2,15 +2,17 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useUserAgentStore } from "@/libs/store/user-agent";
 import { track } from "@vercel/analytics/react";
 
 const CTA = ({ courseId }: { courseId: string }) => {
+  const isMobileApp = useUserAgentStore((state) => state.isMobileApp);
+  const route = isMobileApp
+    ? `/score-card/create/golf-course?golfCourseId=${courseId}`
+    : "/score-card";
   return (
     <Button className="min-w-[320px]" asChild>
-      <Link
-        href={`/score-card/create/golf-course?golfCourseId=${courseId}`}
-        onClick={() => track("create game CTA clicked")}
-      >
+      <Link href={route} onClick={() => track("create game CTA clicked")}>
         파크골프 스코어 기록하기
       </Link>
     </Button>
