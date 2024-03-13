85a7d78967e4c7853dd0998687e0e6cba8739735
toycrane <alwaysfun2183@gmail.com>
2024-03-13 15:58:23 +0900

    feat(components): Add ShareButton component for sharing functionality

--- apps/nextjs/src/app/(routes)/score-card/[id]/completed/layout.tsx
+++ apps/nextjs/src/app/(routes)/score-card/[id]/completed/layout.tsx
@@ -1,10 +1,13 @@
 import BackButton from "@/components/nav/back-button";
 
+import ShareButton from "./_components/share-button";
+
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <main className="h-short-screen grid-rows-score-board grid">
-      <div className="flex items-center gap-1 pt-2">
+      <div className="flex items-center justify-between gap-1 pt-2">
         <BackButton />
+        <ShareButton />
       </div>
       {props.children}
     </main>
