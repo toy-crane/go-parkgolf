f2da30940d5bd99dc4cb85735acace91cbb9107d
toycrane <alwaysfun2183@gmail.com>
2024-03-14 17:19:16 +0900

    refactor(reviews): Remove unnecessary totalAverage calculation

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/reviews.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/reviews.tsx
@@ -9,18 +9,6 @@ import ReviewCard from "./review-card";
 
 const ReviewInfo = async ({ course }: { course: GolfCourse }) => {
   const reviews = await GetReviews(course?.id);
-  // TODO: 리뷰의 갯수가 많아지면 개선
-  const totalAverage =
-    reviews.reduce((acc, review) => {
-      return (
-        acc +
-        (review.course_condition_rating +
-          review.course_difficulty_rating +
-          review.facilities_rating) /
-          3
-      );
-    }, 0) / reviews.length;
-
   return (
     <section className="mb-8 space-y-8">
       {reviews.length === 0 ? (
@@ -30,9 +18,6 @@ const ReviewInfo = async ({ course }: { course: GolfCourse }) => {
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <Icons.starFilled className="mr-1 h-6 w-6" />
-              <span className="mr-2 font-semibold">
-                {totalAverage.toFixed(1)}
-              </span>
               <span>리뷰 {reviews.length}</span>
             </div>
             <CreateReviewButton course={course} />
