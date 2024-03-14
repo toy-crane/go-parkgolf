484dbc0fc1e802b9a7d4c4dda29d50cde760ba4b
toycrane <alwaysfun2183@gmail.com>
2024-03-14 17:14:27 +0900

    feat(routes): Replace button with Feedback component in EmptyReview

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/empty-review.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/_components/empty-review.tsx
@@ -1,25 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import Feedback from "@/components/feedback";
 import type { GolfCourse } from "@/types";
-import Balancer from "react-wrap-balancer";
 
 const EmptyReview = ({ course }: { course: GolfCourse }) => {
-  const router = useRouter();
   return (
-    <div className="my-16 flex flex-col items-center justify-center gap-4 md:my-24">
-      <span className="text-center text-lg font-semibold">
-        <Balancer>소중한 파크골프가자의 첫 리뷰를 작성해주세요</Balancer>
-      </span>
-      <Button
-        variant="outline"
-        onClick={() =>
-          router.push(`/golf-courses/${course.slug}/reviews/create`)
-        }
-      >
-        첫 리뷰 등록하기
-      </Button>
+    <div className="my-16 flex flex-col items-center justify-center gap-4">
+      <Feedback
+        golfCourseSlug={course.slug}
+        label={`${course?.name} 어떠셨나요?`}
+      />
     </div>
   );
 };
