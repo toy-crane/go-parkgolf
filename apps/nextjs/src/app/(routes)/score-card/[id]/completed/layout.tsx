a740a195e736c80ba888e462a1036479cc795e88
toycrane <alwaysfun2183@gmail.com>
2024-03-14 14:48:21 +0900

    style: update layout justify alignment in score card completed page

--- apps/nextjs/src/app/(routes)/score-card/[id]/completed/layout.tsx
+++ apps/nextjs/src/app/(routes)/score-card/[id]/completed/layout.tsx
@@ -5,8 +5,7 @@ import ShareButton from "./_components/share-button";
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <main className="h-short-screen grid-rows-score-board grid">
-      <div className="flex items-center justify-between gap-1 pt-2">
-        <BackButton />
+      <div className="flex items-center justify-end gap-1 pt-2">
         <ShareButton />
       </div>
       {props.children}
