76486d20c8a7e9c2077f2df9f881d7c8083ec468
toycrane <alwaysfun2183@gmail.com>
2024-03-18 17:28:24 +0900

    feat(routes): Add async authentication check in Layout component

--- apps/nextjs/src/app/(routes)/golf-courses/create/layout.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/create/layout.tsx
@@ -1,4 +1,14 @@
-const Layout = (props: { children: React.ReactNode }) => {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { readUserSession } from "@/libs/auth";
+
+const Layout = async (props: { children: React.ReactNode }) => {
+  const session = await readUserSession();
+  const pathname = headers().get("x-pathname") ?? "";
+  if (!session)
+    return redirect(
+      `/login?${new URLSearchParams({ next: pathname }).toString()}`,
+    );
   return <div className="content-grid">{props.children}</div>;
 };
 
