a1a0486c30ce5343dc1e6703ac5414a5c211acf5
toycrane <alwaysfun2183@gmail.com>
2024-03-18 14:24:54 +0900

    feat: Update onClick event to navigate to "/my-games"

--- apps/nextjs/src/components/nav/back-button.tsx
+++ apps/nextjs/src/components/nav/back-button.tsx
@@ -7,7 +7,11 @@ import { ArrowLeft } from "lucide-react";
 const BackButton = () => {
   const router = useRouter();
   return (
-    <Button variant="ghost" size="icon" onClick={() => router.back()}>
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => router.replace("/my-games")}
+    >
       <ArrowLeft size={24} />
     </Button>
   );
