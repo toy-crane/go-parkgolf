2a64c8db05bd4faddc1524a9af863a1e9ccd8b84
toycrane <alwaysfun2183@gmail.com>
2024-03-19 13:00:27 +0900

    feat(routes): Add fetching and displaying game start date in ScoreCard

--- apps/nextjs/src/app/(routes)/score-card/new/[id]/_components/score-card.tsx
+++ apps/nextjs/src/app/(routes)/score-card/new/[id]/_components/score-card.tsx
@@ -1,6 +1,26 @@
+import { createSupabaseServerClient } from "@/libs/supabase/server";
+import { cn } from "@/libs/tailwind";
+import { format } from "date-fns";
+
 const ScoreCard = async ({ gameId }: { gameId: string }) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  return <div>Hello {gameId}</div>;
+  const supabase = await createSupabaseServerClient();
+  const response = await supabase
+    .from("games")
+    .select("started_at")
+    .eq("id", gameId)
+    .single();
+  if (response.error) throw response.error;
+  const { started_at } = response.data;
+  return (
+    <section>
+      <div
+        className={cn("text-muted-foreground flex justify-end pb-1 text-xs")}
+      >
+        {started_at && format(new Date(started_at), "yyyy-MM-dd")}
+      </div>
+    </section>
+  );
 };
 
 export default ScoreCard;
