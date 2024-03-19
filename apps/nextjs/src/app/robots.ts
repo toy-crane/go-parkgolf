250da5ef26769419423923fd4da3e1a86425a926
toycrane <alwaysfun2183@gmail.com>
2024-03-19 12:22:51 +0900

    feat: Add disallow rules for certain URLs in robots file

--- apps/nextjs/src/app/robots.ts
+++ apps/nextjs/src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/api/", "/settings/", "/register/", "/login/", "/auth/"],
     },
     sitemap: "https://www.goparkgolf.app/sitemap.xml",
   };
