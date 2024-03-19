0b5ec6000ac85f68633397ee766e1f800192b51a
toycrane <alwaysfun2183@gmail.com>
2024-03-19 15:07:43 +0900

    feat: Add grid columns and ScoreCardHead component

--- apps/nextjs/src/app/(routes)/score-card/new/[id]/_components/score-card.tsx
+++ apps/nextjs/src/app/(routes)/score-card/new/[id]/_components/score-card.tsx
@@ -1,17 +1,48 @@
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { createSupabaseServerClient } from "@/libs/supabase/server";
 import { cn } from "@/libs/tailwind";
 import { format } from "date-fns";
 
+const gridColumns = {
+  "1": "grid-cols-score-card-1",
+  "2": "grid-cols-score-card-2",
+  "3": "grid-cols-score-card-3",
+  "4": "grid-cols-score-card-4",
+};
+
+const ScoreCardHead = ({
+  label,
+  className,
+}: {
+  label: string;
+  className?: string;
+}) => {
+  return (
+    <TableHead
+      className={cn(
+        "flex h-auto items-center justify-center border px-0 py-2 text-center font-semibold text-black md:px-4",
+        className,
+      )}
+    >
+      {label}
+    </TableHead>
+  );
+};
+
 const ScoreCard = async ({ gameId }: { gameId: string }) => {
   const supabase = await createSupabaseServerClient();
   const response = await supabase
     .from("games")
-    .select("started_at, game_courses(*)")
+    .select("started_at, game_courses(*), game_players(id, nickname)")
     .eq("id", gameId)
     .single();
   if (response.error) throw response.error;
-  const { started_at, game_courses: gameCourses } = response.data;
+  const {
+    started_at,
+    game_courses: gameCourses,
+    game_players: players,
+  } = response.data;
 
   return (
     <section>
@@ -34,7 +65,24 @@ const ScoreCard = async ({ gameId }: { gameId: string }) => {
         </TabsList>
         {gameCourses.map((gc) => (
           <TabsContent value={gc.name} key={gc.id} className="flex-1">
-            {gc.name} 코스
+            <Table className="flex h-full flex-1 flex-col text-xs md:text-sm">
+              <TableHeader className="flex-0">
+                <TableRow
+                  className={cn(
+                    "align-center grid",
+                    gridColumns[
+                      String(players.length) as keyof typeof gridColumns
+                    ],
+                  )}
+                >
+                  <ScoreCardHead label="홀" className="bg-lime-200" />
+                  <ScoreCardHead label="파" className="bg-lime-400" />
+                  {players.map((player) => (
+                    <ScoreCardHead key={player.id} label={player.nickname} />
+                  ))}
+                </TableRow>
+              </TableHeader>
+            </Table>
           </TabsContent>
         ))}
       </Tabs>
