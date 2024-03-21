8379daa2324c196e24b59f6e64edcd7d3f4d96c6
toycrane <alwaysfun2183@gmail.com>
2024-03-21 18:50:54 +0900

    style: Update bottom-cta background styling

--- apps/nextjs/src/app/(routes)/score-card/create/_components/bottom-cta.tsx
+++ apps/nextjs/src/app/(routes)/score-card/create/_components/bottom-cta.tsx
@@ -11,7 +11,7 @@ const BottomCTA = ({
   loading: boolean;
 }) => {
   return (
-    <div className="bottom-cta content-grid">
+    <div className="bottom-cta content-grid bg-gradient-to-t from-white from-80% to-transparent pt-5">
       <Button type="submit" size="lg" disabled={loading || disabled}>
         {loading ? (
           <Loader2 className="h-5 w-5 animate-spin" size={24} />
