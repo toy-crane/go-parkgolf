10ad34bb03eca9c502000bb6326992d2d7e47041
toycrane <alwaysfun2183@gmail.com>
2024-03-27 15:38:29 +0900

    feat: Add download banner component and user agent validation

--- apps/nextjs/src/app/(routes)/trending/layout.tsx
+++ apps/nextjs/src/app/(routes)/trending/layout.tsx
@@ -1,13 +1,19 @@
+import { headers } from "next/headers";
+import DownloadBanner from "@/components/app/download-banner";
 import BottomNav from "@/components/nav/bottom";
 import { PageHeader, PageHeaderHeading } from "@/components/page-header";
+import { isApp } from "@/libs/user-agent";
 import { TrendingUp } from "lucide-react";
 
 import Products from "../../../components/ad/products";
 import { TrendingNav } from "./nav";
 
 const Layout = (props: { children: React.ReactNode }) => {
+  const headersList = headers();
+  const userAgent = headersList.get("user-agent")!;
   return (
     <>
+      <DownloadBanner isApp={isApp(userAgent)} />
       <main className="content-grid pb-[var(--bottom-nav-height)]">
         <div className="pb-28">
           <PageHeader className="relative flex flex-row items-center pb-4 md:pb-6">
