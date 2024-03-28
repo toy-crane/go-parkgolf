b487b9bc79d1376ec60d39422ab80c23c120cffa
toycrane <alwaysfun2183@gmail.com>
2024-03-28 18:53:20 +0900

    feat(routes): Add metadata generation function for region page

--- apps/nextjs/src/app/(routes)/regions/[z_code]/page.tsx
+++ apps/nextjs/src/app/(routes)/regions/[z_code]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata, ResolvingMetadata } from "next";
 import Link from "next/link";
 import BreadcrumbNav from "@/components/nav/breadcrumb-nav";
 import {
@@ -5,8 +6,62 @@ import {
   PageHeaderDescription,
   PageHeaderHeading,
 } from "@/components/page-header";
+import { siteConfig } from "@/config/site";
 import { createSupabaseServerClientReadOnly } from "@/libs/supabase/server";
 import _ from "lodash";
+
+interface Props {
+  params: { z_code: string; zs_code: string };
+}
+
+export async function generateMetadata(
+  { params: { z_code } }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const regionName = decodeURIComponent(z_code);
+  const supabase = await createSupabaseServerClientReadOnly();
+  const response = await supabase
+    .rpc("get_region_2depth_count", {
+      region_1depth_name: regionName,
+    })
+    .select("*");
+  if (response.error) throw response.error;
+  const regions = response.data;
+  const totalCount = _.sumBy(regions, "count");
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images ?? [];
+
+  if (totalCount) {
+    const title = `${regionName}의 모든 파크골프장 리스트`;
+    const description = `
+      ${regionName}에는 ${totalCount.toLocaleString()}개의 파크골프장이 있습니다. 
+      보다 자세한 정보가 궁금하다면? 파크골프가자 홈페이지에서 확인하세요.
+      전국 모든 파크 골프장 검색 및 주변 정보, 스코어 기록까지 가능합니다.
+    `;
+    const url = `${siteConfig.url}/regions/${z_code}`;
+    return {
+      title,
+      description,
+      openGraph: {
+        title,
+        description,
+        url,
+        images: [...previousImages],
+      },
+      twitter: {
+        title,
+        description,
+        images: [...previousImages],
+      },
+      alternates: {
+        canonical: url,
+      },
+    };
+  } else {
+    return {};
+  }
+}
 
 const Page = async ({
   params,
