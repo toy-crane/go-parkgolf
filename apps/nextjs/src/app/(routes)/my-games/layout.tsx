0d78ef948b9d4dc5b7b382022dfeb82ee4522984
toycrane <alwaysfun2183@gmail.com>
2024-04-02 16:49:14 +0900

    feat: Add dynamic import for DownloadBanner component

--- apps/nextjs/src/app/(routes)/my-games/layout.tsx
+++ apps/nextjs/src/app/(routes)/my-games/layout.tsx
@@ -1,13 +1,26 @@
+import dynamic from "next/dynamic";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import BottomNav from "@/components/nav/bottom";
 import { readUserSession } from "@/libs/auth";
+import { isApp } from "@/libs/user-agent";
+
+const DownloadBanner = dynamic(
+  () => import("@/components/app/download-banner"),
+  {
+    ssr: false,
+  },
+);
 
 const Layout = async (props: { children: React.ReactNode }) => {
   const session = await readUserSession();
   if (!session) return redirect("/score-card");
+  const headersList = headers();
+  const userAgent = headersList.get("user-agent")!;
 
   return (
     <>
+      <DownloadBanner isApp={isApp(userAgent)} />
       <main className="content-grid pb-[var(--bottom-nav-height)]">
         {props.children}
       </main>
