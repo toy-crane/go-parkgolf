c326373d4e99ed31a456f7ec225004c6610adde6
toycrane <alwaysfun2183@gmail.com>
2024-04-04 15:24:45 +0900

    feat(routes): add ReviewInfo component and related functionalities

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/reviews/page.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/reviews/page.tsx
@@ -1,5 +1,47 @@
-const Page = () => {
-  return <div>review</div>;
+import { Icons } from "@/components/icons";
+import { Separator } from "@/components/ui/separator";
+import { createSupabaseServerClientReadOnly } from "@/libs/supabase/server";
+import type { GolfCourse } from "@/types";
+
+import CreateReviewButton from "../_components/create-review-button";
+import EmptyReview from "../_components/empty-review";
+import NaverReviews from "../_components/naver-review";
+import ReviewCard from "../_components/review-card";
+import { GetReviews } from "../action";
+
+const ReviewInfo = async ({ params }: { params: { slug: string } }) => {
+  const supabase = await createSupabaseServerClientReadOnly();
+  const slug = decodeURIComponent(params.slug);
+  const response = await supabase
+    .from("golf_courses")
+    .select("*")
+    .eq("slug", slug)
+    .returns<GolfCourse[]>()
+    .single();
+  if (response.error) throw response.error;
+  const course = response.data;
+  const reviews = await GetReviews(course.id);
+  return (
+    <div className="min-h-[25vh] space-y-6">
+      {reviews.length === 0 ? (
+        <EmptyReview course={course} />
+      ) : (
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <Icons.starFilled className="mr-1 h-6 w-6" />
+              <span>리뷰 {reviews.length}</span>
+            </div>
+            <CreateReviewButton course={course} />
+          </div>
+          {reviews.map((review) => (
+            <ReviewCard review={review} key={review.id} />
+          ))}
+        </div>
+      )}
+      <NaverReviews courseName={course.name} />
+    </div>
+  );
 };
 
-export default Page;
+export default ReviewInfo;
