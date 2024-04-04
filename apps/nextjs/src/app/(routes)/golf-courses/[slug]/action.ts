efe3236ca00a27bc912677b3185c74b5573504ba
toycrane <alwaysfun2183@gmail.com>
2024-04-04 16:23:55 +0900

    refactor(routes): Remove unused imports and refactor functions

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/action.ts
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/action.ts
@@ -1,9 +1,6 @@
 "use server";
 
-import {
-  createSupabaseServerClient,
-  createSupabaseServerClientReadOnly,
-} from "@/libs/supabase/server";
+import { createSupabaseServerClientReadOnly } from "@/libs/supabase/server";
 
 export const GetReviews = async (golfCourseId: string) => {
   const supabase = await createSupabaseServerClientReadOnly();
@@ -13,23 +10,4 @@ export const GetReviews = async (golfCourseId: string) => {
     .eq("golf_course_id", golfCourseId);
   if (response.error) throw response.error;
   return response.data;
-};
-
-export const createGolfCourseQnA = async (data: {
-  golfCourseId: string;
-  text: string;
-  parentId: string;
-  level: number;
-}) => {
-  const { golfCourseId, text, parentId, level } = data;
-  const supabase = await createSupabaseServerClient();
-  const response = await supabase.from("golf_course_qnas").insert({
-    golf_course_id: golfCourseId,
-    parent_id: parentId,
-    content: text,
-    level: level + 1,
-  });
-  if (response.error) {
-    throw response.error;
-  }
 };
