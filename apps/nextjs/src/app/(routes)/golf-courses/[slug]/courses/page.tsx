ae6fe0ea0ca5dc61324388d8e9f2b27224817083
toycrane <alwaysfun2183@gmail.com>
2024-04-05 18:20:27 +0900

    feat: Add CourseTable component for golf courses page

--- apps/nextjs/src/app/(routes)/golf-courses/[slug]/courses/page.tsx
+++ apps/nextjs/src/app/(routes)/golf-courses/[slug]/courses/page.tsx
@@ -1,41 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { generateFormUrl } from "@/libs/google-form";
 import { createSupabaseServerClientReadOnly } from "@/libs/supabase/server";
-import { Pencil } from "lucide-react";
 
 import { getCourse } from "../fetcher";
-
-const EmptyCourse = ({ courseName }: { courseName: string }) => {
-  return (
-    <div className="flex min-h-[30vh] items-center justify-center">
-      <div className="space-y-4 text-center">
-        <div className="text-lg font-bold">
-          아직 등록된 코스 정보가 없습니다.
-        </div>
-        <Button variant="outline" asChild>
-          <a
-            href={generateFormUrl(courseName)}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="ml-2"
-          >
-            빠른 등록 요청하기 <Pencil size={16} className="ml-2" />
-          </a>
-        </Button>
-      </div>
-    </div>
-  );
-};
+import CourseTable from "./_components/course-table";
 
 const Page = async ({ params }: { params: { slug: string } }) => {
   const supabase = await createSupabaseServerClientReadOnly();
@@ -63,7 +30,6 @@ const Page = async ({ params }: { params: { slug: string } }) => {
     0,
   );
   const hasCourses = courses.length > 0;
-  const defaultValue = courses[0]?.name!;
   return (
     <div className="min-h-[25vh] space-y-6">
       <div className="space-y-3">
@@ -94,67 +60,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
         </div>
         <Separator />
       </div>
-      <div className="space-y-3">
-        <h2 className="text-foreground text-xl font-bold">코스 상세</h2>
-        {hasCourses ? (
-          <Tabs defaultValue={defaultValue} className="mb-28 space-y-3">
-            <TabsList className="flex flex-nowrap justify-start overflow-x-scroll">
-              {courses.map((course) => (
-                <TabsTrigger
-                  value={course.name}
-                  key={course.id}
-                  className="flex-1"
-                >
-                  {course.name} 코스
-                </TabsTrigger>
-              ))}
-            </TabsList>
-            {courses.map((course) => (
-              <TabsContent
-                value={course.name}
-                className="min-h-[25vh]"
-                key={course.id}
-              >
-                <Table className="flex flex-col">
-                  <TableHeader className="flex">
-                    <TableRow className="grid-cols-course-detail grid flex-1">
-                      <TableHead className="bg-muted flex h-8 w-[100px] items-center justify-center font-semibold">
-                        홀
-                      </TableHead>
-                      <TableHead className="flex h-8 items-center justify-center text-center font-semibold">
-                        파
-                      </TableHead>
-                      <TableHead className="flex h-8 items-center justify-center text-center font-semibold">
-                        거리
-                      </TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody className="flex flex-col">
-                    {course.holes.map((hole) => (
-                      <TableRow
-                        key={hole.id}
-                        className="grid-cols-course-detail grid flex-1"
-                      >
-                        <TableCell className="bg-muted flex h-6 w-[100px] items-center justify-center text-center">
-                          {hole.hole_number} 홀
-                        </TableCell>
-                        <TableCell className="flex h-6 items-center justify-center text-center">
-                          {hole.par}
-                        </TableCell>
-                        <TableCell className="flex h-6 items-center justify-center text-center">
-                          {hole.distance} M
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TabsContent>
-            ))}
-          </Tabs>
-        ) : (
-          <EmptyCourse courseName={golfCourse.name} />
-        )}
-      </div>
+      <CourseTable courses={courses} golfCourse={golfCourse} />
     </div>
   );
 };
